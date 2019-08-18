3ddaca6086b2f7da7dd67285a6e46ab9228a9a55
Anand pratap singh <47928185+singhanand453@users.noreply.github.com>
2019-08-18 16:09:34 +0530

    Update combobox.py

--- combobox.py
+++ combobox.py
@@ -2,15 +2,15 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox
 window=Tk()
-window.geometry("400x300")
-window.title("combobox")
+window.geometry("500x400")
+window.title("combo-box")
 label1=Label(window,text="enter your name : ")
-label1.grid(row=0,column=1,pady="3")
+label1.grid(row=0,column=1,pady="2")
 entry=Entry(window)
-entry.grid(row=0,column=2,pady="3",ipady="1")
+entry.grid(row=0,column=2,pady="2",ipady="1")
 
-label=Label(window,text="select your city : ")
-label.grid(row=2,column=1,pady="3")
+label=Label(window,text="select city : ")
+label.grid(row=2,column=1,pady="2")
 
 combobox=Combobox(window,state="readonly",values=("noida","gonda","lucknow","agra","delhi"),background="yellow")
 combobox.grid(row=2,column=2,pady="2",ipady="1")
