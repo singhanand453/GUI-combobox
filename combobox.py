bdb469b5ab5a89d938b2b1afb6110ef12fd59b25
Anand pratap singh <47928185+singhanand453@users.noreply.github.com>
2019-08-25 23:16:44 +0530

    Update combobox.py

--- combobox.py
+++ combobox.py
@@ -4,20 +4,20 @@ from tkinter import messagebox
 window=Tk()
 window.geometry("500x400")
 window.title("combo-box")
-label1=Label(window,text="enter your name : ")
+label1=Label(window,text="enter name : ")
 label1.grid(row=0,column=1,pady="2")
 entry=Entry(window)
 entry.grid(row=0,column=2,pady="2",ipady="1")
 
-label=Label(window,text="select city : ")
+label=Label(window,text="select your  city : ")
 label.grid(row=2,column=1,pady="2")
 
-combobox=Combobox(window,state="readonly",values=("noida","gonda","lucknow","agra","delhi"),background="yellow")
+combobox=Combobox(window,state="readonly",values=("noida","gonda","lucknow","agra","delhi"),background="gray")
 combobox.grid(row=2,column=2,pady="2",ipady="1")
 def submit_button():
-    messagebox.showinfo("Message","Your city: "+combobox.get())
+    messagebox.showinfo("Mess-age","Your city: "+combobox.get())
 
-button=Button(window,text="submit",command=submit_button)
+button=Button(window,text="sub-mit",command=submit_button)
 button.grid(row=3,column=2)
 
 window.mainloop()
